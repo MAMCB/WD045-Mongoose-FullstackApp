9fe0b1726825a8ffe24d55652efe427d19866018
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-01-15 10:54:51 +0100

    fixed routes error

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,10 +3,10 @@ const express = require("express");
 const eventRouter = express.Router();
 const {createEvent,getAllEvents,getEventById,updateEventById,deleteEventById} = require('../controllers/events');
 
-router.post("/api/events",createEvent);
-router.get("/api/events",getAllEvents);
-router.get("/api/events/:id",getEventById);
-router.put("/api/events/:id",updateEventById);
-router.delete("/api/events/:id",deleteEventById);
+eventRouter.post("/api/events", createEvent);
+eventRouter.get("/api/events", getAllEvents);
+eventRouter.get("/api/events/:id", getEventById);
+eventRouter.put("/api/events/:id", updateEventById);
+eventRouter.delete("/api/events/:id", deleteEventById);
 
 module.exports = eventRouter;
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,11 +4,11 @@ const userRouter = express.Router();
 const {createUser,getAllUsers,getUserById,updateUserById,deleteUserById} = require('../controllers/users');
 
 
-router.post("/api/users",createUser);
-router.get("/api/users",getAllUsers);
-router.get("/api/users/:id",getUserById);
-router.put("/api/users/:id",updateUserById);
-router.delete("/api/users/:id",deleteUserById);
+userRouter.post("/api/users",createUser);
+userRouter.get("/api/users", getAllUsers);
+userRouter.get("/api/users/:id", getUserById);
+userRouter.put("/api/users/:id", updateUserById);
+userRouter.delete("/api/users/:id", deleteUserById);
 
 module.exports = userRouter;
 
